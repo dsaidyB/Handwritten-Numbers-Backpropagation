c40ef967405e006c408b45c228db9d2c6b9551d4
Dhyey Bhatt <dhyey.bhatt@gmail.com>
2025-06-22 03:28:19 -0400

    added first layer of backpropagation

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -61,20 +61,22 @@ bias_vector_1 = matrixFromFile('layer1Biases.txt')
 # layer 1 (16 x 784 matrix), multiply by input vector, subtract bias, pass this through sigmoid/relu
 layer_1_matrix = matrixFromFile('layer1Weights.txt')
 
-# bias vector 2
+# bias vector 2 (16 x 1 matrix)
 bias_vector_2 = matrixFromFile('layer2Biases.txt')
 # layer 2 (16 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
 layer_2_matrix = matrixFromFile('layer2Weights.txt')
 
-# bias vector 3
+# bias vector 3 (10 x 1 matrix)
 bias_vector_output = matrixFromFile('outputLayerBiases.txt')
 # output layer (10 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
 output_layer_matrix  = matrixFromFile('outputLayerWeights.txt')
 
 
-for i in range(1000):
+for i in range(100):
     gradient_list = []
     for image in images:
+        gradient_components = []
+
         # read image in grayscale mode
         # img = cv2.imread("setupImage.png", 0)
         img = cv2.imread(image, 0) 
@@ -88,12 +90,15 @@ for i in range(1000):
 
         # weights say pattern of pixels that is important, bias says how active these pixels need to be
         # layer 1 output (16 x 1 matrix)
-        layer_1_output = relu(np.matmul(layer_1_matrix, input_vector) - bias_vector_1)
+        layer_1_output = np.matmul(layer_1_matrix, input_vector) - bias_vector_1
+        layer_1_processed = relu(layer_1_output)
         # layer 2 output (16 x 1 matrix)
-        layer_2_output = relu(np.matmul(layer_2_matrix, layer_1_output) - bias_vector_2)
+        layer_2_output = np.matmul(layer_2_matrix, layer_1_processed) - bias_vector_2
+        layer_2_processed = relu(layer_2_output)
         # output vector (10 x 1 matrix)
-        output_vector = softmax(np.matmul(output_layer_matrix, layer_2_output) - bias_vector_output)
-        print(output_vector)
+        output_vector = np.matmul(output_layer_matrix, layer_2_processed) - bias_vector_output
+        final_output_vector = softmax(output_vector)
+        print(final_output_vector)
 
         labelled_number = int(image.split("/")[-1][0])
         true_output_vector = np.zeros(10)
@@ -103,40 +108,66 @@ for i in range(1000):
         print(true_output_vector)
 
 
-        output_vector = output_vector.flatten()
+        final_output_vector = final_output_vector.flatten()
         eps = 1e-12
-        loss = -np.log(output_vector[labelled_number] + eps)    # cross-entropy
+        loss = -np.log(final_output_vector[labelled_number] + eps)    # cross-entropy
         print(loss)
 
 
         # use chain rule relationships to get the gradient for the given image
+        bias_vector_output_gradient = []
+        output_layer_matrix_gradient = []
 
-        gradient_list.append([])
+        bias_vector_2_gradient = []
+        layer_2_matrix_gradient = []
+
+        bias_vector_1_gradient = []
+        layer_1_matrix_gradient = []
+
+        for j in range(0, 10):
+            dLoss_dZj_L = final_output_vector[j]-true_output_vector[j]
+            
+            dZj_L_dbj_L = 1
+            dLoss_dbj_L = dLoss_dZj_L * dZj_L_dbj_L
+            bias_vector_output_gradient.append(dLoss_dbj_L)
+
+            for i in range(0, 16):
+                dZj_L_dwji_L = layer_2_processed[i]
+                dLoss_dwji_L = dLoss_dZj_L * dZj_L_dwji_L
+                output_layer_matrix_gradient.append(dLoss_dwji_L)
+
+                dZj_L_dai_L1 = output_layer_matrix[j][i]
+                dLoss_dai_L1 = dLoss_dZj_L * dZj_L_dai_L1
+            
+
+        gradient_components = bias_vector_1_gradient + bias_vector_2_gradient + bias_vector_output_gradient + layer_1_matrix_gradient + layer_2_matrix_gradient + output_layer_matrix_gradient
+
+        gradient_list.append(np.array(gradient_components))
          
 
     # then i do this for all images in a training data subset, take the average of them
     gradient_matrix = np.stack(gradient_list)        # Convert list of vectors into a 2D array
-    gradient_descent = np.mean(gradient_matrix, axis=0)
+    gradient_descent = -1*np.mean(gradient_matrix, axis=0)
 
     # nudge weights by some scaling factor of negative gradient
     alpha = 0.05
 
     bias_vector_1 += alpha * gradient_descent[0:16]  # first 16 are for bias vector 1
-    bias_vector_2 += alpha * gradient_descent[16:32]
-    bias_vector_output += alpha * gradient_descent[32:48]
+    bias_vector_2 += alpha * gradient_descent[16:32] # second 16 are for bias vector 2
+    bias_vector_output += alpha * gradient_descent[32:42] # third 10 are for bias vector output
 
     for i in range(len(layer_1_matrix)):
-        lower_bound = 48+i*len(layer_1_matrix[i])
+        lower_bound = 42+i*len(layer_1_matrix[i])
         upper_bound = lower_bound + len(layer_1_matrix[i])
         layer_1_matrix[i] += alpha * gradient_descent[lower_bound : upper_bound]
 
     for j in range(len(layer_2_matrix)):
-        lower_bound = 48+len(layer_1_matrix)*len(layer_1_matrix[0])  +  j*len(layer_2_matrix[j])
+        lower_bound = 42+len(layer_1_matrix)*len(layer_1_matrix[0])  +  j*len(layer_2_matrix[j])
         upper_bound = lower_bound + len(layer_2_matrix[j])
         layer_2_matrix[j] += alpha * gradient_descent[lower_bound : upper_bound]
 
     for k in range(len(output_layer_matrix)):
-        lower_bound = 48+len(layer_1_matrix)*len(layer_1_matrix[0])+len(layer_2_matrix)*len(layer_2_matrix[0])  +  k*len(output_layer_matrix) 
+        lower_bound = 42+len(layer_1_matrix)*len(layer_1_matrix[0])+len(layer_2_matrix)*len(layer_2_matrix[0])  +  k*len(output_layer_matrix) 
         upper_bound = lower_bound + len(output_layer_matrix[j])
         output_layer_matrix[k] += alpha * gradient_descent[lower_bound : upper_bound]
 
