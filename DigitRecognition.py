f3c27007028620f69d04f93ba1e5912bc36c4a78
Dhyey Bhatt <dhyey.bhatt@gmail.com>
2025-06-22 00:29:01 -0400

    added updating data pipelines, relu, softmax other functions

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -38,53 +38,112 @@ def matrixFromFile(file_path):
     
     return np.array(matrix)
 
+def saveMatrix(matrix, file_path):
+    # Write each row to the file, comma-separated
+    with open(file_path, 'w') as file:
+        for row in matrix:
+            line = ','.join(str(num) for num in row)
+            file.write(line + '\n')
+    # print(f"Matrix saved to {file_path}")
 
-# read image in grayscale mode
-# img = cv2.imread("setupImage.png", 0)
+def relu(vector):
+    return np.maximum(0, vector)
 
-for image in images:
-    img = cv2.imread(image, 0) 
+def softmax(vector):
+    vector = vector.flatten() # Ensure it's 1D
+    exps = np.exp(vector - np.max(vector))      # since exponent the relative sizes will be same (can factor out the largest thing), so like can just initially subtract the largest since then have to do smaller exponents
+    return exps / np.sum(exps)
 
-    # normalize the img values between 0 and 1
-    normalized_img = np.round(img / 255.0, decimals=5)
+# load weights and biases
 
-    # convert the image into a input vector (784 x 1 matrix)
-    input_vector = normalized_img.flatten().reshape(-1,1) # -1 is a filler, 1 is saying make it column vector
+# bias vector 1 (16 x 1 matrix)
+bias_vector_1 = matrixFromFile('layer1Biases.txt')
+# layer 1 (16 x 784 matrix), multiply by input vector, subtract bias, pass this through sigmoid/relu
+layer_1_matrix = matrixFromFile('layer1Weights.txt')
 
+# bias vector 2
+bias_vector_2 = matrixFromFile('layer2Biases.txt')
+# layer 2 (16 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
+layer_2_matrix = matrixFromFile('layer2Weights.txt')
 
-    # weights say pattern of pixels that is important, bias says how active these pixels need to be
-
-
-    # bias vector 1 (16 x 1 matrix)
-    bias_vector_1 = matrixFromFile('layer1Biases.txt')
-    # layer 1 (16 x 784 matrix), multiply by input vector, subtract bias, pass this through sigmoid/relu
-    layer_1_matrix = np.random.rand(16, 784)  # Values from 0 to 1
-    # layer 1 output (16 x 1 matrix)
-    layer_1_output = np.matmul(layer_1_matrix, input_vector) - bias_vector_1
-
-
-    # bias vector 2
-    bias_vector_2 = np.random.rand(16, 1)  # Values from 0 to 1
-    # layer 2 (16 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
-    layer_2_matrix = np.random.rand(16, 16)
-    # layer 2 output (16 x 1 matrix)
-    layer_2_output = np.matmul(layer_2_matrix, layer_1_output) - bias_vector_2
-
-    # bias vector 3
-    bias_vector_output = np.random.rand(10, 1)  # Values from 0 to 1
-    # output layer (10 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
-    output_layer_matrix  = np.random.rand(10, 16)  # Values from 0 to 1
-    # output vector (10 x 1 matrix)
-    output_vector = np.matmul(output_layer_matrix, layer_2_output) - bias_vector_output
-
-    print(output_vector)
+# bias vector 3
+bias_vector_output = matrixFromFile('outputLayerBiases.txt')
+# output layer (10 x 16 matrix), multiply by new input vector, subtract bias, pass this through sigmoid/relu
+output_layer_matrix  = matrixFromFile('outputLayerWeights.txt')
 
 
-    # ok so then i need to subtract from desired value, take squared error
-    # use chain rule relationships to get the gradient for the given image
+for i in range(1000):
+    gradient_list = []
+    for image in images:
+        # read image in grayscale mode
+        # img = cv2.imread("setupImage.png", 0)
+        img = cv2.imread(image, 0) 
+
+        # normalize the img values between 0 and 1
+        normalized_img = np.round(img / 255.0, decimals=5)
+
+        # convert the image into a input vector (784 x 1 matrix)
+        input_vector = normalized_img.flatten().reshape(-1,1) # -1 is a filler, 1 is saying make it column vector
+
+
+        # weights say pattern of pixels that is important, bias says how active these pixels need to be
+        # layer 1 output (16 x 1 matrix)
+        layer_1_output = relu(np.matmul(layer_1_matrix, input_vector) - bias_vector_1)
+        # layer 2 output (16 x 1 matrix)
+        layer_2_output = relu(np.matmul(layer_2_matrix, layer_1_output) - bias_vector_2)
+        # output vector (10 x 1 matrix)
+        output_vector = softmax(np.matmul(output_layer_matrix, layer_2_output) - bias_vector_output)
+        print(output_vector)
+
+        labelled_number = int(image.split("/")[-1][0])
+        true_output_vector = np.zeros(10)
+        true_output_vector[labelled_number] = 1
+
+        print(labelled_number)
+        print(true_output_vector)
+
+
+        output_vector = output_vector.flatten()
+        eps = 1e-12
+        loss = -np.log(output_vector[labelled_number] + eps)    # cross-entropy
+        print(loss)
+
+
+        # use chain rule relationships to get the gradient for the given image
+
+        gradient_list.append([])
+         
 
     # then i do this for all images in a training data subset, take the average of them
+    gradient_matrix = np.stack(gradient_list)        # Convert list of vectors into a 2D array
+    gradient_descent = np.mean(gradient_matrix, axis=0)
 
-    # nudge weights by some scaling factor of negative gradient 
+    # nudge weights by some scaling factor of negative gradient
+    alpha = 0.05
+
+    bias_vector_1 += alpha * gradient_descent[0:16]  # first 16 are for bias vector 1
+    bias_vector_2 += alpha * gradient_descent[16:32]
+    bias_vector_output += alpha * gradient_descent[32:48]
+
+    for i in range(len(layer_1_matrix)):
+        lower_bound = 48+i*len(layer_1_matrix[i])
+        upper_bound = lower_bound + len(layer_1_matrix[i])
+        layer_1_matrix[i] += alpha * gradient_descent[lower_bound : upper_bound]
+
+    for j in range(len(layer_2_matrix)):
+        lower_bound = 48+len(layer_1_matrix)*len(layer_1_matrix[0])  +  j*len(layer_2_matrix[j])
+        upper_bound = lower_bound + len(layer_2_matrix[j])
+        layer_2_matrix[j] += alpha * gradient_descent[lower_bound : upper_bound]
+
+    for k in range(len(output_layer_matrix)):
+        lower_bound = 48+len(layer_1_matrix)*len(layer_1_matrix[0])+len(layer_2_matrix)*len(layer_2_matrix[0])  +  k*len(output_layer_matrix) 
+        upper_bound = lower_bound + len(output_layer_matrix[j])
+        output_layer_matrix[k] += alpha * gradient_descent[lower_bound : upper_bound]
 
 
+saveMatrix(bias_vector_1, 'layer1Biases.txt')
+saveMatrix(bias_vector_2, 'layer2Biases.txt')
+saveMatrix(bias_vector_output, 'outputLayerBiases.txt')
+saveMatrix(layer_1_matrix, 'layer1Weights.txt')
+saveMatrix(layer_2_matrix, 'layer2Weights.txt')
+saveMatrix(output_layer_matrix, 'outputLayerWeights.txt')
